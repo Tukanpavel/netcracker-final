06c8a9ba98a36e21a3a535bb8115179e5793ec64
Pavel Mastyaev <psmas.pm@gmail.com>
2019-07-26 16:14:18 +0400

    Added login module with navigation bar, login module doesnt do requests

--- src/front/src/app/app.module.ts
+++ src/front/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from "../material-module/material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  exports:[
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/front/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+  });
+
+  is_clicked: boolean=false;
+
+  submit() {
+    if (this.form.valid&&!this.is_clicked) {
+      this.is_clicked=true;
+      this.submitEM.emit(this.form.value);
+    }
+  }
+
+  @Output() submitEM = new EventEmitter();
+
+  getUsernameErrorMessage(){
+    return 'Write username!';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.form.get('password').value==='') return "Password is required to sign in.";
+    else return 'Password must be six characters or longer.'
+  }
+}
