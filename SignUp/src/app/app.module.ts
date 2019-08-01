e6c84c184ff1cfd9d5655821519166266435b459
Alexandr <sashka.filippov@gmail.com>
2019-08-01 11:25:13 +0400

    WIP: added error alert message (+ ERROR 403)

--- SignUp/src/app/app.module.ts
+++ SignUp/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatStepperModule, MatInputModule, MatButtonModule} from '@angular/material'
+import {MatStepperModule, MatInputModule, MatButtonModule, MatCardModule} from '@angular/material'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import {MatCardModule} from '@angular/material'
-import {HttpClientModule} from "@angular/common/http";
 import { UserComponent } from './sign-up/user/user.component';
-
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
--- SignUp/src/app/sign-up/sign-up.component.ts
+++ SignUp/src/app/sign-up/sign-up.component.ts
@@ -20,26 +20,9 @@ export class SignUpComponent implements OnInit {
   signUp(event: User) {
     return this.http.post<User>(environment.url+'/registration', event).subscribe(() => {
       alert("You have been signed up successfully");
-    });
+    }, error1 => alert("Error: it was unable to signup!"));
 
     console.log('signup() - SUCCESS');
   }
-
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
 
 }
