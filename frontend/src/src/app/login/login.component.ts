efe5902ac30f96dd5961b1f998a670c12ed1224b
Pavel Mastyaev <psmas.pm@gmail.com>
2019-08-02 16:21:31 +0400

    added post request

--- frontend/src/src/app/login/login.component.ts
+++ frontend/src/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {User} from "../sign-up/user/user";
 import {LoginInfo} from "../login-info";
 import {LoginService} from "../login.service";
 
@@ -10,26 +9,26 @@ import {LoginService} from "../login.service";
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent {
+
+  constructor(private loginService: LoginService){ }
+
   form: FormGroup = new FormGroup({
     username: new FormControl('',[Validators.required]),
     password: new FormControl('',[Validators.required,Validators.minLength(6)]),
   });
 
-
-
-
-  is_clicked: boolean=false;
+  user=new LoginInfo();
+  isClicked: boolean=false;
 
   submit() {
-    if (this.form.valid&&!this.is_clicked) {
-      this.is_clicked=true;
-      this.submitEM.emit(this.form.value);
-      //this.loginService.postLogin(this.user).subscribe(x=>console.log('Observer got a next value: ' + x),err => console.error('Observer got an error: ' + err),
-        //() => console.log('Observer got a complete notification'))
+    if (this.form.valid && !this.isClicked) {
+      this.isClicked=true;
+      this.user=this.form.value;
+      this.loginService.postLogin(this.user).subscribe(x=>console.log('Observer got a next value: ' + x),err => console.error('Observer got an error: ' + err),
+        () => console.log('Observer got a complete notification'))
+      console.log(this.user);
     }
   }
-
-  @Output() submitEM = new EventEmitter <User>();
 
   getUsernameErrorMessage(){
     return 'Write username!';
