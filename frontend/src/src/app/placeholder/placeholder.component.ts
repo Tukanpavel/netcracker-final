f937e0fb7957d657c0ebf6cc8cb457a348698d40
Pavel Mastyaev <psmas.pm@gmail.com>
2019-08-08 13:03:26 +0400

    Adding untracked files

---
+++ frontend/src/src/app/placeholder/placeholder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'NCvito-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.less']
+})
+export class PlaceholderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/src/app/services/auth.guard.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {LoginService} from "../login/login.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(
+    private router:Router,
+    private loginService:LoginService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    let url: string = state.url;
+
+    return this.checkLogin(url);
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.loginService.isLoggedIn()) { return true; }
+
+    // Store the attempted URL for redirecting
+    this.loginService.redirectUrl = url;
+
+    // Navigate to the login page with extras
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
